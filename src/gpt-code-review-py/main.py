69a4c58a6751dd37e531f11e12ca7d3b97f3c562
Piotr Stradowski <stradowski.stradek@gmail.com>
2023-08-03 22:09:14 +0200

    Add command arguments

--- src/gpt-code-review-py/main.py
+++ src/gpt-code-review-py/main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from datetime import datetime
 
 import openai
@@ -6,9 +7,6 @@ import openai
 from data_management import get_data_file_path, get_data_content, get_json_file
 
 from github_utils import get_git_pr
-
-repo_url = "https://github.com/Stradek/DX12EngineTest"
-pr_id = 18
 
 def get_required_data(config):
   ai_review_template_path = get_data_file_path(config, "ai_review_template")
@@ -38,7 +36,11 @@ def get_ai_code_review(openai_model, system_prompt, pull_request):
   response = completion.choices[0].message["content"]
   return response
 
-def main():
+def main(args):
+  pr_id = args.pr_id
+  repo_url = args.repo_url
+  output = args.output
+
   root_dir = os.getcwd()
 
   config_filepath = os.path.join(root_dir, "config.json")
@@ -58,19 +60,28 @@ def main():
   print(ai_code_review)
   
   # if there is no output directory - create one
-  try:
-    output_dir = config["output_file"].split("/")[0]
-    output_dir_path = os.path.join(root_dir, output_dir)
-    os.mkdir(output_dir_path)
-  except:
-    pass
+  if output:
+    output_file_path=output
+  else:
+    try:
+      output_dir = config["output_file"].split("/")[0]
+      output_dir_path = os.path.join(root_dir, output_dir)
+      os.mkdir(output_dir_path)
+    except:
+      pass
 
-  datetime_str = "{0:%Y}-{0:%m}-{0:%d}-{0:%H}-{0:%M}-{0:%S}".format(datetime.now())
-  output_file_path = os.path.join(root_dir, config["output_file"])
-  output_file_path = output_file_path.replace("{datetime}", datetime_str)
+    datetime_str = "{0:%Y}-{0:%m}-{0:%d}-{0:%H}-{0:%M}-{0:%S}".format(datetime.now())
+    output_file_path = os.path.join(root_dir, config["output_file"])
+    output_file_path = output_file_path.replace("{datetime}", datetime_str)
 
   with open(output_file_path, "x") as output_file:
      output_file.write(ai_code_review)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="GPT Code Review Py")
+    parser.add_argument("-i", "--pr_id", type=int, help="Pull Request ID", required=True)
+    parser.add_argument("-r", "--repo_url", type=str, help="Github Repository URL", required=True)
+    parser.add_argument("-o", "--output", type=str, help="Output File name", required=False)
+
+    args = parser.parse_args()
+    main(args)
